4663d429340a50c193be02c712c032786ad71721
Katie <k@as-MacBook-Air.local>
2025-07-04 12:46:55 -0700

    Get API call working async with api key, params, handle response

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,37 @@
 import express from 'express';
-import { v1 } from '@googlemaps/places';
+import { Client } from '@googlemaps/google-maps-services-js';
+import 'dotenv/config';
 const app = express();
-const { PlacesClient } = v1
-const placesClient = new PlacesClient();
+const googleClient = new Client({});
 const port = 3000;
+let GOOGLE_API_KEY = "";
+if (process.env.GOOGLE_API_KEY) {
+  GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+} else {
+  throw new Error('Could not load GOOGLE_API_KEY');
+}
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.get('/nearby-stores', async (req, res) => {
-  const locationRestriction = {};
-  const request = {
-    locationRestriction,
-  };
-  
-  const response = await placesClient.searchNearby(request);
-  console.log(response);
+  async function getNearbyStores() {
+    try {
+      const response = await googleClient.placesNearby({
+        params: {
+          location: { lat: 37.8715, lng: -122.2730 }, // Example: San Francisco
+          radius: 1500, // in meters
+          keyword: "boba", // optional: filter by keyword
+          key: GOOGLE_API_KEY,
+        }
+      });
+      res.json(response.data.results);
+    } catch (error) {
+      res.statusCode(500).send({ error: 'Failure!' });
+    }
+  }
+  await getNearbyStores();
 });
 
 app.listen(port, () => {
